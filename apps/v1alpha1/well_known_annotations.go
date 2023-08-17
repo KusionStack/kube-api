8863bce804de57767db667a20e076a2d96186115
zoumo <jim.zoumo@gmail.com>
2023-08-17 16:57:22 +0800

    feat: sync ruleset api

--- apps/v1alpha1/well_known_annotations.go
+++ apps/v1alpha1/well_known_annotations.go
@@ -19,5 +19,7 @@ package v1alpha1
 const (
 	PodAvailableConditionsAnnotation = "pod.kusionstack.io/available-conditions" // indicate the available conditions of a pod
 
+	AnnotationPodSkipRuleConditions = "ruleset.kusionstack.io/skip-rule-conditions"
+
 	LastPodStatusAnnotationKey = "collaset.kusionstack.io/last-pod-status"
 )
