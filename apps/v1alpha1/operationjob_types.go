d0eef055208b431790fd6bda5e8e5c24a5346fb2
hexin <574252631@qq.com>
2024-08-09 17:34:45 +0800

    add operationjob progress; rename oj to opj (#15)
    * add operationjob progress; rename oj to opj
    
    * rename FinishingOpsLifecycle

--- apps/v1alpha1/operationjob_types.go
+++ apps/v1alpha1/operationjob_types.go
@@ -35,10 +35,11 @@ const (
 type OperationProgress string
 
 const (
-	OperationProgressPending    OperationProgress = "Pending"
-	OperationProgressProcessing OperationProgress = "Processing"
-	OperationProgressFailed     OperationProgress = "Failed"
-	OperationProgressSucceeded  OperationProgress = "Succeeded"
+	OperationProgressPending               OperationProgress = "Pending"
+	OperationProgressProcessing            OperationProgress = "Processing"
+	OperationProgressFinishingOpsLifecycle OperationProgress = "FinishingOpsLifecycle"
+	OperationProgressFailed                OperationProgress = "Failed"
+	OperationProgressSucceeded             OperationProgress = "Succeeded"
 )
 
 // OperationJobSpec defines the desired state of OperationJob
