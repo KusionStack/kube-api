03af2146e073870b0e45b9c8439e8c1fdc9bb63a
youngLiuHY <qyliu1996@163.com>
2025-09-02 14:54:40 +0800

    feat: add rollout annotations key && AnnoRolloutPhaseRollbacking

--- rollout/well_known_annotations.go
+++ rollout/well_known_annotations.go
@@ -37,6 +37,8 @@ const (
 
 	// AnnoRolloutName is the name of the rollout object.
 	AnnoRolloutName = "rollout.kusionstack.io/name"
+
+	AnnoRolloutPhaseRollbacking = "rollout.kusionstack.io/rollbacking"
 )
 
 // new style key
