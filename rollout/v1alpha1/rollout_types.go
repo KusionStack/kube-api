ced8934525ff43ae3bd32f1b04f8c12766a85965
zoumo <jim.zoumo@gmail.com>
2025-12-18 19:44:03 +0800

    refactor(rollout): make replicas fields optional in status

--- rollout/v1alpha1/rollout_types.go
+++ rollout/v1alpha1/rollout_types.go
@@ -181,15 +181,19 @@ type RolloutBatchStatus struct {
 
 type RolloutReplicasSummary struct {
 	// Replicas is the desired number of pods targeted by workload
+	// +optional
 	Replicas int32 `json:"replicas"`
 	// AvailableReplicas is the number of service available pods targeted by workload.
 	// +optional
 	AvailableReplicas int32 `json:"availableReplicas"`
 	// UpdatedReplicas is the number of pods targeted by workload that have the updated template spec.
+	// +optional
 	UpdatedReplicas int32 `json:"updatedReplicas"`
 	// UpdatedReadyReplicas is the number of ready pods targeted by workload that have the updated template spec.
+	// +optional
 	UpdatedReadyReplicas int32 `json:"updatedReadyReplicas"`
 	// UpdatedAvailableReplicas is the number of service available pods targeted by workload that have the updated template spec.
+	// +optional
 	UpdatedAvailableReplicas int32 `json:"updatedAvailableReplicas"`
 }
 
